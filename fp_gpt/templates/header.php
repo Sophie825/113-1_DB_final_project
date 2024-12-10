a6a832a8767b2cf83615f266c6863b112d7b3652
sophie <sophiehuang0825@gmail.com>
2024-12-11 01:46:02 +0800

    fix header 2

--- fp_gpt/templates/header.php
+++ fp_gpt/templates/header.php
@@ -1,10 +1,8 @@
 <nav>
     <?php if (isset($_SESSION['role'])): ?>
         <span>歡迎，<?= htmlspecialchars($_SESSION['username']); ?>！</span>
-        {% comment %} <a href="dashboard.php">主頁</a> {% endcomment %}
         <a href="logout.php">登出</a>
     <?php else: ?>
-        {% comment %} <a href="index.php">登入</a> {% endcomment %}
     <?php endif; ?>
 </nav>
 <hr>
