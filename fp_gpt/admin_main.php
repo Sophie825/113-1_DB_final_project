a54de7eea715ff090754201e4d9966132072ad9f
sophie <sophiehuang0825@gmail.com>
2024-12-11 03:13:49 +0800

    fix class path

--- fp_gpt/admin_main.php
+++ fp_gpt/admin_main.php
@@ -40,12 +40,12 @@ include_once 'db/db.php';
                     <button class="click_in"><a href="modules/mentor.php">進入</a></button>
                 </div>
                 <div class="option-card">
-                    <h3>輔導老師資料管理</h3>
+                    <h3>輔導老師排班資料管理</h3>
                     <button class="click_in"><a href="modules/edit_mentor_shift.php">進入</a></button>
                 </div>
                 <div class="option-card">
                     <h3>班級資料管理</h3>
-                    <button class="click_in"><a href="modules/classroom.php">進入</a></button>
+                    <button class="click_in"><a href="modules/class.php">進入</a></button>
                 </div>
                 <div class="option-card">
                     <h3>試聽資料管理</h3>
